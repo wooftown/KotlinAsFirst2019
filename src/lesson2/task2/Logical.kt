8fd308c3fb7bc4287d6d6289355dcc8db1f44bdb
woof <wooftown@yandex.ru>
2019-09-05 17:21:24 +0300

    Logic solved not all

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
 
 /**
  * Пример
@@ -12,13 +13,15 @@ import lesson1.task1.sqr
 fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
     sqr(x - x0) + sqr(y - y0) <= sqr(r)
 
+
 /**
  * Простая
  *
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int) = number / 1000 + (number / 100) % 10 == (number / 10) % 10 + number % 10
+
 
 /**
  * Простая
@@ -27,7 +30,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int) =
+    x1 == x2 || y1 == y2 || (abs(x1 - x2) == abs(y1 - y2))
 
 
 /**
@@ -36,7 +40,27 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    return when (month) {
+        1 -> 31
+        2 -> when {
+            year % 4 == 0 && year % 100 != 0 -> 29
+            year % 400 == 0 -> 29
+            else -> 28
+        }
+        3 -> 31
+        4 -> 30
+        5 -> 31
+        6 -> 30
+        7 -> 31
+        8 -> 31
+        9 -> 30
+        10 -> 31
+        11 -> 30
+        12 -> 31
+        else -> 404
+    }
+}
 
 /**
  * Средняя
@@ -44,11 +68,13 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
  * Проверить, лежит ли окружность с центром в (x1, y1) и радиусом r1 целиком внутри
  * окружности с центром в (x2, y2) и радиусом r2.
  * Вернуть true, если утверждение верно
+ * r1*r1>(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+r2*r2
  */
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
 ): Boolean = TODO()
+
 
 /**
  * Средняя
@@ -57,6 +83,8 @@ fun circleInside(
  * Стороны отверстия должны быть параллельны граням кирпича.
  * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
- * Вернуть true, если кирпич пройдёт
+ * Вернуть true, если кирпич пройдёт ab ac ba bc ca cb
+ *
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+    ((a <= r)) && (b <= s) || ((a <= r) && (c <= s)) || ((b <= r) && (a <= s)) || ((b <= r) && (c <= s)) || ((c <= r) && (a <= s)) || ((c <= r) && (b <= s))
