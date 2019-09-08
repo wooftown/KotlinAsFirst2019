8d34cf8530c4384441314b1d9ead4e87a853cdb8
woof <wooftown@yandex.ru>
2019-09-08 19:51:43 +0300

    fix fix fix

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -79,7 +79,7 @@ fun circleInside(
  *
  */
 fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
-    val a = min(min(a, b), c)
-    val b = a + b + c - a - max(max(a, b), c)
-    return a <= min(r,s) && b <= max(r,s)
+    val x = min(min(a, b), c)
+    val y = a + b + c - x - max(max(a, b), c)
+    return x <= min(r, s) && y <= max(r, s)
 }
